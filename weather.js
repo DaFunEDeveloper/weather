6c1beec7a42a9b4459d9fd696573cf3c852f3a8a
DaFunEDeveloper <62800011+DaFunEDeveloper@users.noreply.github.com>
2020-04-14 18:53:21 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,10 @@
 "use strict"
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q=pontiac&appid=ad0ad46e12935e5d07614976f2b10ef1"
+
+$.ajax ({
+	url: url,
+	success: function (result){
+		console.log(result);
+	}
+})
